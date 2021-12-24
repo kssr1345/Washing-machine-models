ee3f397a915f4c668e7659c058c9fc3fcd1b02c7
kssr1345 <39305041+kssr1345@users.noreply.github.com>
2021-12-24 16:39:53 +0530

    Add files via upload

---
+++ build/default/production/clcd.p1.d
@@ -0,0 +1,6 @@
+build/default/production/clcd.p1:  \
+clcd.c  \
+main.h  \
+clcd.h  \
+digital_keypad.h  \
+timers.h 
---
+++ build/default/production/digital_keypad.p1.d
@@ -0,0 +1,6 @@
+build/default/production/digital_keypad.p1:  \
+digital_keypad.c  \
+main.h  \
+clcd.h  \
+digital_keypad.h  \
+timers.h 
